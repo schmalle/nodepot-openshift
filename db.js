e02c52b16a36cd8b17dac742154d86095094963c
Markus Schmall <schmall.markus@googlemail.com>
2015-05-01 09:54:41 +0200

    fixed twitter bug

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 /*
 
-    Database related routines
+   Database related routines
 
  */
 
@@ -12,22 +12,21 @@ var configGlobal = "none";
 
 function initClient() {
 
-    var client = "";
 
     var openShiftIP = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
     if (openShiftIP != undefined)
     {
         console.log("Starting DB client with ip/port server " + process.env.OPENSHIFT_REDIS_PORT + " : " + process.env.OPENSHIFT_REDIS_HOST + " on openshift");
-        client = redis.createClient(process.env.OPENSHIFT_REDIS_PORT, process.env.OPENSHIFT_REDIS_HOST);
+        var client = redis.createClient(process.env.OPENSHIFT_REDIS_PORT, process.env.OPENSHIFT_REDIS_HOST);
         client.auth(process.env.REDIS_PASSWORD);
+        return client;
 
     }
     else {
         console.log("Starting DB without special parameters outside of OpenShift environment ...");
-        client = redis.createClient();
+        var client = redis.createClient();
+        return client;
     }
-
-    return client;
 
 }
 

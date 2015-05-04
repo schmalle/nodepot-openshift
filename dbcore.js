2ddd4ac937eeb4630966dc3229d209aa2d6a4a6a
Markus Schmall <schmall.markus@googlemail.com>
2015-05-04 21:00:15 +0200

    fixed twitter bug

--- dbcore.js
+++ dbcore.js
@@ -1,18 +1,21 @@
 var redis = require("redis");
 
+var port = process.env.OPENSHIFT_REDIS_PORT;
 
-function initClient() {
-
+function initClient()
+{
     var openShiftIP = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
-    if (openShiftIP != undefined) {
-        console.log("Starting DB client with ip/port server " + process.env.OPENSHIFT_REDIS_PORT + " : " + process.env.OPENSHIFT_REDIS_HOST + " on openshift");
+    if (openShiftIP != undefined)
+    {
+        console.log("Starting DB client with ip/port server " + port + " : " + process.env.OPENSHIFT_REDIS_HOST + " on openshift");
 
-        var client = redis.createClient(process.env.OPENSHIFT_REDIS_PORT, process.env.OPENSHIFT_REDIS_HOST);
+        var client = redis.createClient(port, process.env.OPENSHIFT_REDIS_HOST);
         client.auth(process.env.REDIS_PASSWORD);
         return client;
 
     }
-    else {
+    else
+    {
         console.log("Starting DB without special parameters outside of OpenShift environment ...");
         var client = redis.createClient();
         return client;
